d22427764a3c4072049500cd966d9fd6ec634487
jhonny-jnc <jhonny.jnc80@gmail.com.mx>
2018-11-12 17:05:40 -0500

    Upload data

---
+++ js/sw-utils.js
@@ -0,0 +1,14 @@
+function updateCacheDynamic(dynamicCache,req,res)
+{
+    if(res.ok)
+    {
+        return caches.open(dynamicCache)
+            .then(cache => {
+                cache.put(req,res.clone());
+                return res.clone();
+            });
+    }
+    else{
+        
+    }
+}
